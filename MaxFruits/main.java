bf838f174ba384f384d8343b88815a3482aaffab
Edgar Jr. San Martin <edgar.sanmartinjr@gmail.com>
2020-07-18 04:15:32 -0400

    Update main.java

--- MaxFruits/main.java
+++ MaxFruits/main.java
@@ -1,3 +1,16 @@
+/*
+  T - O(n), because we are looping through the char[] once.
+  S - O(1), because the HashMap is the data structure effecting the space efficiency of this function and it is only keeping track of up to two kvp aka 2 baskets
+
+  Given an array of characters where each character represents a fruit tree, you are given two baskets and your goal is to put maximum number
+  of fruits in each basket. The only restriction is that each basket can have only one type of fruit.
+
+  You can start with any tree, but once you have started you can’t skip a tree. You will pick one fruit from each tree until you cannot, i.e.,
+  you will stop when you have to pick from a third fruit type.
+
+  Write a function to return the maximum number of fruits in both the baskets.
+*/
+
 import java.util.*;
 
 class MaxFruitCountOf2Types {
