18a05612c0a61856b960a8cef51c408351f5c81e
Edgar Jr. San Martin <edgar.sanmartinjr@gmail.com>
2020-07-22 10:37:51 -0400

    Added time, space complexity analysis and question

--- RemoveDuplicates/main.java
+++ RemoveDuplicates/main.java
@@ -1,3 +1,11 @@
+/*
+  T -  O(n), since we have to loop through every element in the array
+  S - O(1), since we are only ever creating the lastNonDuplicating int variable every time this function is called
+  
+  Given an array of sorted numbers, remove all duplicates from it. You should not use any extra space; after removing
+  the duplicates in-place return the new length of the array.
+*/
+
 class RemoveDuplicates {
 
   public static int remove(int[] arr) {
