bd77119a9465920d26b975b620eadf62b649950a
Edgar Jr. San Martin <edgar.sanmartinjr@gmail.com>
2020-07-22 09:38:59 -0400

    Create main.java

---
+++ PairWithTargetSum/main.java
@@ -0,0 +1,23 @@
+class PairWithTargetSum {
+
+  public static int[] search(int[] arr, int targetSum){
+    int[] answer = new int[2];
+    int startPointer = 0;
+    int endPointer = arr.length - 1;
+    int currentSum = 0;
+
+    while(startPointer < endPointer){
+      currentSum = arr[startPointer] + arr[endPointer];
+      if(currentSum == targetSum){
+        answer[0] = startPointer;
+        answer[1] = endPointer;
+        break;
+      } else if(currentSum > targetSum){
+        endPointer--;
+      } else if(currentSum < targetSum){
+        startPointer++;
+      }
+    }
+    return answer;
+  }
+}
